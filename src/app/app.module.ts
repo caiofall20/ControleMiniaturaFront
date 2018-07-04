10c76603ef29296fcc5852d3b5b2e81eaee996e4
Caio Mateus <caiomateusgames@outlook.com>
2018-07-04 12:19:01 -0300

    Subindo o front com a tela de pesquisa das miniaturas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import { AppComponent } from './app.component';
+import { Button, ButtonModule } from 'primeng/components/button/button';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
